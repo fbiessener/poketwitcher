b289b797f70d06833a7e36ab35f623ce61d67ae6
vagrant <vagrant@vagrant.vm>
2020-04-01 17:24:41 +0000

    Styling

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -2,21 +2,30 @@
 
 // Show Search Results
 
-function replaceSearchResults(res) {
-    $("#search-info").html(res);
-    console.log(res);
-}
+// function replaceSearchResults(res) {
+//     $("#search-info").html(res);
+//     console.log(res);
+// }
 
-function showSearchResults(evt) {
-    evt.preventDefault();
+// function showSearchResults(evt) {
+//     evt.preventDefault();
 
-    let url = "/search.json";
-    let formData = {"results": $("#search-field").val()};
+//     let url = "/search.json";
+//     let formData = {"results": $("#search-field").val()};
 
-    $.get(url, formData, replaceSearchResults(formData.results));
-}
+//     $.get(url, formData, replaceSearchResults(formData.results));
+// }
 
 $("#search-form").on('submit', showSearchResults);
+
+$('#search-form').on('submit', (evt) => {
+  evt.preventDefault();
+  let url = '/search.json';
+  const formValues = $('#search-field').serialize();
+  $.get(url, formValues, (response) => {
+    $('#search-info').text(response);
+  });
+});
 
 // SERVER-SIDE
 
